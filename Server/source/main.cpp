acdb6d99a65e5437733f91d9b1f994ae5a59df0b
jyoushitou <xzt20060906@outlook.com>
2026-09-21 15:03:45 +0800

    delete NetHttp

--- Server/source/main.cpp
+++ Server/source/main.cpp
@@ -1,10 +1,8 @@
-/*
- * @file        main.cpp
- * @brief       服务器入口（启动 TCP 服务、处理退出信号）
- * @author      jyoushitou
- * @date        2026-09-16
- * @copyright   Copyright (c) 2026
- */
+/// @file        main.cpp
+/// @brief       服务器入口（启动 TCP 服务、处理退出信号）
+/// @author      jyoushitou
+/// @date        2026-09-16
+/// @copyright   Copyright (c) 2026
 
 // Server/source/main.cpp
 #include "NetServer.h"
